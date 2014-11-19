496ae88b8acc33170460caa21394ab43539774dc
Neel Shah <neel@nsdesigners.com>
2014-11-19 03:11:58 -0500

    Logically this method simply overrides the wpm_signup_user function from ms-functions.php when called by user-new.php and does follow the correct process to achieve the desired output however for some reason currently the function/filter is not overriding the default wpm_signup_user function and so don't work. I tried by adding test data to the function to see if it works but didn't do anything.

--- skip-activation-email.php
+++ skip-activation-email.php
@@ -6,7 +6,7 @@ Plugin URI: http://wp.nyu.edu
 Author: Neel Shah <shah.neel@nyu.edu>
 Author URI: http://neelshah.info
 License: GPL2
-Version: 0.1
+Version: 0.3
 */
 
 if( !class_exists( 'Skip_Activation_Email_NSD' ) ) {
@@ -14,16 +14,33 @@ if( !class_exists( 'Skip_Activation_Email_NSD' ) ) {
 
         /* Construct the plugin object  */
         public function __construct() {
-            /* Add filter to remove sending activation email link when site administrators add new users to their sites */
             add_filter( 'wpmu_signup_user_notification', '__return_false', 1 ); // Disable confirmation email
-
-            add_action( 'user_new_form', array( $this , 'custom_fields_below_add_new_user' ) );
+            
+            /* Add filter to remove sending activation email link when site administrators add new users to their sites */
+            add_filter( 'wpmu_signup_user', array( $this, 'wpmu_signup_user_and_skip_activation_email', 1 ) );
         }
-        public function custom_fields_below_add_new_user() {
-            echo '<p>Test Field</p>';
+        public function wpmu_signup_user_and_skip_activation_email( $user, $user_email, $meta = array() ) {
+            global $wpdb;
 
-            echo '<label for="noconfirmation"><input type="checkbox" name="noconfirmation" id="noconfirmation" value="1" /> Add the user without sending an email that requires their confirmation.</label>';
-        }        
+            // Format data
+            $user = preg_replace( '/\s+/', '', sanitize_user( $user, true ) );
+            $user_email = sanitize_email( $user_email );
+            $key = substr( md5( time() . rand() . $user_email ), 0, 16 );
+            $meta = serialize($meta);
+
+            $wpdb->insert( $wpdb->signups, array(
+                'domain' => '',
+                'path' => '',
+                'title' => '',
+                'user_login' => $user,
+                'user_email' => $user_email,
+                'registered' => current_time('mysql', true),
+                'activation_key' => $key,
+                'meta' => $meta
+            ) );
+            
+            wpmu_activate_signup( $key );
+        }
     }
 }
 
